cb324ac501578d6bc955533b79a54a7c0a0cf472
SaumyaGuptaTechnokrat <65768914+SaumyaGuptaTechnokrat@users.noreply.github.com>
2023-10-13 19:34:11 +0530

    Collection Removed

--- ShuffleArray.java
+++ ShuffleArray.java
@@ -1,16 +1,22 @@
 package shuffleArray;
-
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 public class ShuffleArray {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Integer[] arr = { 1, 2, 3, 4, 5, 6, 7 };//declaring and intializing an array with 7 values
-		List<Integer> lists = Arrays.asList(arr);/*The Arrays.asList() method is used to convert the arr array into a List of Integers*/
-		Collections.shuffle(lists);//to shuffle all the elements
-		lists.toArray(arr);//covertin lists to arrays
-		System.out.println(Arrays.toString(arr));//printing the array
-	}
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void shuffleArray(Integer[] arr) {
+        int n = arr.length;
+        for (int i = n - 1; i > 0; i--) {
+            int j = (int) (Math.random() * (i + 1));
+            // Swap arr[i] and arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
 }
